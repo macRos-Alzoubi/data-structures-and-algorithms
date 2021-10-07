ab099da8d9f89189ebd820f4d31e507efff7c153
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-10-07 22:18:13 +0300

    fin code challemge #10

---
+++ java/StacksAndQueues/app/src/main/java/Queue/Queue.java
@@ -0,0 +1,63 @@
+package Queue;
+
+import Node.Node;
+
+public class Queue<T> {
+
+  private Node<T> front;
+  private Node<T> back;
+
+  public Queue() {
+    front = back = null;
+  }
+
+  public void enqueue(T data) {
+    Node<T> node = new Node<>(data);
+    if (isEmpty())
+      front = node;
+    else {
+      back.setNext(node);
+    }
+    back = node;
+  }
+
+  public T dequeue() throws Exception {
+    T data;
+    if(isEmpty())
+      throw new Exception("Can't remove from empty Queue!");
+    else{
+      data = front.getData();
+      front = front.getNext();
+    }
+    return data;
+  }
+
+  public T peek() throws Exception {
+    if(isEmpty())
+      throw new Exception("Can't peek empty Queue!");
+    else
+      return front.getData();
+  }
+
+  public boolean isEmpty() {
+    return front == null;
+  }
+
+  @Override
+  public String toString() {
+    String queueStr = getQueueString();
+    return String.format("back -> %s <- front",queueStr);
+  }
+
+  private String getQueueString() {
+    StringBuilder queueStr = new StringBuilder();
+    if(!isEmpty()){
+      Node<T> current = front;
+      while(current != null){
+        queueStr.append(current.getData()).append(" ");
+        current = current.getNext();
+      }
+    }
+    return queueStr.toString();
+  }
+}
---
+++ java/StacksAndQueues/app/src/main/java/Stack/Stack.java
@@ -0,0 +1,62 @@
+package Stack;
+
+import Node.Node;
+
+public class Stack<T> {
+  private Node<T> top;
+
+  public Stack() {
+    top = null;
+  }
+
+  public Node<T> getTop() {
+    return top;
+  }
+
+  public void push(T data) {
+    Node<T> node = new Node<>(data);
+    if (!isEmpty())
+      node.setNext(top);
+    top = node;
+  }
+
+  public T pop() throws Exception {
+    T data;
+    if(isEmpty())
+      throw new Exception("Can't remove from empty Stack!");
+    else{
+      data = top.getData();
+      top = top.getNext();
+    }
+    return data;
+  }
+
+  public T peek() throws Exception {
+    if(isEmpty())
+      throw new Exception("Can't peek empty Stack!");
+    else
+      return top.getData();
+  }
+
+  public boolean isEmpty() {
+    return top == null;
+  }
+
+  @Override
+  public String toString() {
+    String stackStr = getStackString();
+    return String.format("Top -> %s ->NULL",stackStr);
+  }
+
+  private String getStackString() {
+    StringBuilder StackStr = new StringBuilder();
+    if(!isEmpty()){
+      Node<T> current = top;
+      while(current != null){
+        StackStr.append(current.getData()).append(" ");
+        current = current.getNext();
+      }
+    }
+    return StackStr.toString();
+  }
+}
