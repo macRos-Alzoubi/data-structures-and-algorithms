42b477c27d4160f25f9885b2988a14a1ee53f09f
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-11-04 19:25:05 +0200

    fin code-30

---
+++ java/hashTable/README.md
@@ -0,0 +1,14 @@
+# Hashtables
+<!-- Short summary or background information -->
+Hashtable is key value data structure that use hash function that takes a key and give you an index the index is used to store and get value.
+
+## Challenge
+<!-- Description of the challenge -->
+HashTable implementation in java
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Big O{ Space: O(n), Time: O(n)}
+
+## API
+<!-- Description of each method publicly available in each of your hashtable -->
---
+++ java/hashTable/app/src/main/java/hashTable/structure/HashTable.java
@@ -0,0 +1,107 @@
+package hashTable.structure;
+
+import hashTable.data.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class HashTable<K, V> {
+
+  private List<Node<K, V>> bucketArray;
+  private int size;
+  private int bucketSize;
+
+  public HashTable(){
+    bucketArray = new ArrayList<>();
+    size = 0;
+    bucketSize = 10;
+
+    listInit();
+  }
+
+  private void listInit() {
+    for(int i = 0; i < bucketSize; i++)
+      bucketArray.add(null);
+  }
+
+  public V get(K key) throws Exception {
+    if(isEmpty())
+      throw  new Exception("Can't get from empty table");
+    else{
+      int index = hash(key);
+      int hashCode = hashCode(key);
+      Node<K, V> head = bucketArray.get(index);
+
+      while (head != null){
+        if(key.equals(head.getKey()) && hashCode == head.getHashCode())
+          return head.getValue();
+        head = head.getNext();
+      }
+      return null;
+    }
+  }
+
+  public void add(K key, V value) throws Exception {
+    int index = hash(key);
+    int hashCode = hashCode(key);
+    Node<K, V> head = bucketArray.get(index);
+
+    while (head != null){
+      if(head.getKey().equals(key) && head.getHashCode() == hashCode) {
+        head.setValue(value);
+        return;
+      }
+      head = head.getNext();
+    }
+
+    size++;
+    Node<K, V> newNode = new Node<>(key, value, hashCode);
+    head = bucketArray.get(index);
+
+    newNode.setNext(head);
+    bucketArray.set(index, newNode);
+
+    if ((1.0 * size) / bucketSize >= 0.7) {
+      List<Node<K, V> > temp = bucketArray;
+      bucketArray = new ArrayList<>();
+      bucketSize = 2 * bucketSize;
+      size = 0;
+      for (int i = 0; i < bucketSize; i++)
+        bucketArray.add(null);
+
+      for (Node<K, V> headNode : temp) {
+        while (headNode != null) {
+          add(headNode.getKey(), headNode.getValue());
+          headNode = headNode.getNext();
+        }
+      }
+    }
+
+  }
+
+  public boolean contains(K key) throws Exception {
+    return get(key) != null;
+  }
+
+  private int hash(K key){
+    int hashCode = Objects.hashCode(key);
+    hashCode = hashCode < 0 ? hashCode * -1 : hashCode;
+    return hashCode % bucketSize;
+  }
+
+  public final int hashCode (K key) {
+    return Objects.hashCode(key);
+  }
+
+  public int size(){ return size;}
+
+  public boolean isEmpty(){ return size() == 0;}
+
+  @Override
+  public String toString() {
+    return "HashTable{" +
+      "bucketArray=" + bucketArray +
+      '}';
+  }
+}
